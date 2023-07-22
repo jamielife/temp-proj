ce819aa6045c2c8e23e7c9b58aae31206d0f523b
Jamie Taylor <jvptaylor@hotmail.com>
2023-07-21 23:42:52 -0400

    Initial Commit w Issue

---
+++ app/modal.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform } from 'react-native';
+import { View, Text } from 'native-base'
+
+export default function ModalScreen() {
+  return (
+    <View>
+      <Text>Modal</Text>
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
+}
