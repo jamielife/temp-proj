1620d0a56f9196d148fa01365b6c7d249961fcc6
Jamie Taylor <jvptaylor@hotmail.com>
2023-07-22 03:51:27 +0000

    Native Base import in Modal.tsx

--- app/modal.tsx
+++ app/modal.tsx
@@ -7,7 +7,7 @@ export default function ModalScreen() {
     <View>
       <Text>Modal</Text>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'}  />
     </View>
   );
 }
